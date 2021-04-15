e203f0cbcb54a3ca846232a9ca322e7d36d25913
Rydal <i.am.alex2k@gmail.com>
2021-04-16 00:32:07 +0300

    Equipments: added updating, creation and multi-destroy

---
+++ src/views/Equipment/Constructor.vue
@@ -0,0 +1,125 @@
+<template>
+  <div class="section">
+    <h1 class="mb-5">{{ pageName }}</h1>
+    <b-form>
+      <b-card class="mb-3 shadow-sm" header="Основное">
+        <b-form-group label="Название"
+                      label-for="name"
+                      :invalid-feedback="getValidationMessage($v.formData.name)"
+        >
+          <b-form-input id="name"
+                        v-model.trim="formData.name"
+                        :state="getFieldState($v.formData.name)"
+          />
+        </b-form-group>
+        <b-form-group label="Тип"
+                      label-for="type"
+                      :invalid-feedback="getValidationMessage($v.formData.realty_type_id)"
+        >
+          <b-select
+              id="type"
+              v-model="formData.realty_type_id"
+              :state="getFieldState($v.formData.realty_type_id)"
+          >
+            <b-select-option :value="null">--- Выберите тип ---</b-select-option>
+            <b-select-option v-for="(type, idx) in types"
+                             :key="idx"
+                             :value="type.id"
+            >
+              {{ type.name }}
+            </b-select-option>
+          </b-select>
+        </b-form-group>
+      </b-card>
+    </b-form>
+    <ConstructorActions :cancel-to="{ name: 'admin.equipment' }" :is-create-page="isCreatePage" @submit="onSubmit"/>
+  </div>
+</template>
+
+<script lang="ts">
+import {Component, Mixins, Watch} from "vue-property-decorator";
+import ConstructorHelpers from "@/mixins/constructorHelpers";
+import {Validation, validationMixin} from "vuelidate";
+import ValidationMixin from "@/mixins/validation";
+import {required} from "vuelidate/lib/validators";
+import ConstructorActions from "@/components/widget/ConstructorActions.vue";
+import RealtyType from "@/models/RealtyType";
+import {getModule} from "vuex-module-decorators";
+import Notification from "@/store/modules/notification";
+import UploadedImage from "@/components/UploadedImage.vue";
+import Equipment from "@/models/Equipment";
+
+
+@Component({
+  components: {UploadedImage, ConstructorActions},
+  validations: {
+    formData: {
+      name: {
+        required
+      },
+      realty_type_id: {
+        required
+      }
+    }
+  }
+})
+export default class Constructor extends Mixins<Validation, ValidationMixin, ConstructorHelpers>(validationMixin, ValidationMixin, ConstructorHelpers) {
+  entityName = 'комплектация недвижимости'
+  types = [] as Array<RealtyType>
+
+  formData = {
+    id: null as null | number,
+    name: '',
+    realty_type_id: null as null | number
+  }
+
+  onSubmit(redirect = false): void {
+    this.$v.$touch()
+
+    if (!this.$v.$invalid) {
+      if (this.isCreatePage) {
+        Equipment.create(this.formData)
+            .then((response) => {
+              getModule(Notification, this.$store).setData({title: 'Запись успешно создана', variant: 'success'})
+              this.$router.push({name: 'admin.equipment.change', params: {id: response.data.id as unknown as string}})
+            })
+      } else {
+        Equipment.update(this.formData)
+            .then((response) => {
+              getModule(Notification, this.$store).setData({title: 'Запись успешно изменена', variant: 'success'})
+              this.updateFormData(response.data)
+            })
+      }
+
+      if (redirect) {
+        this.$router.push({name: 'admin.equipment'})
+      }
+
+    } else {
+      getModule(Notification, this.$store).setData({
+        title: 'Ошибка валидации!',
+        text: 'Проверте корректность и запоолненость полей',
+        variant: 'danger'
+      })
+    }
+  }
+
+  updateFormData (equipment: Equipment): void {
+    this.formData = { name: equipment.name as string, id: equipment.id as number, realty_type_id: equipment.realty_type_id as number }
+  }
+
+  created(): void {
+    RealtyType.getList().then(response => {
+      this.types = response.data
+    })
+    if (!this.isCreatePage) {
+      Equipment.get({id: this.$route.params.id as unknown as number})
+          .then(response => this.updateFormData(response.data))
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Equipment/Index.vue
@@ -0,0 +1,121 @@
+<template>
+  <div class="section">
+    <h1 class="mb-5">Комплектация</h1>
+
+    <b-card class="mb-4 shadow-sm">
+      <div class="d-flex">
+        <b-button variant="primary" class="mr-2" :to="{ name: 'admin.equipment.create' }">Создать</b-button>
+        <b-button variant="info" class="mr-3" @click="onSelectAll">{{ selectionBtnText }}</b-button>
+        <b-button variant="danger" class="my-2 my-sm-0" :disabled="selectedAllRows" @click="onDelete">Удалить выбранное</b-button>
+      </div>
+    </b-card>
+    <b-card class="shadow-sm">
+      <b-form inline class="mb-3" @submit.prevent="omSearch">
+        <b-form-input v-model="tableTemp.searchValue" class="mr-sm-2" placeholder="Поиск"></b-form-input>
+        <b-select v-model="tableTemp.searchField" class="mr-sm-3">
+          <b-select-option :value="null">
+            --- Выберите поле для поиска ---
+          </b-select-option>
+          <b-select-option v-for="(field, idx) in fields"
+                           v-if="field.searchable"
+                           :key="idx"
+                           :value="field.key"
+          >
+            {{ field.label }}
+          </b-select-option>
+        </b-select>
+        <b-button variant="outline-primary" class="my-2 my-sm-0" type="submit">Найти</b-button>
+      </b-form>
+      <b-table
+          :fields="fields"
+          :items="items"
+          responsive="md"
+          select-mode="multi"
+          striped
+          hover
+          selectable
+          @row-selected="onRowSelected"
+          @sort-changed="onChangeSort"
+          ref="table"
+          :busy="inRequestState"
+      >
+        <template #cell(selected)="{ rowSelected }">
+          <template v-if="rowSelected">
+            <span>&check;</span>
+          </template>
+          <template v-else>
+            <span>&nbsp;</span>
+          </template>
+        </template>
+        <template #cell(name)="{ item }">
+          <b-link :to="{ name: 'admin.equipment.change', params: { id: item.id } }" v-html="tableOptions.searchValue ? getValueWithSearchPart(item.name, tableOptions.searchValue) : item.name "></b-link>
+        </template>
+      </b-table>
+    </b-card>
+  </div>
+</template>
+
+<script lang="ts">
+import {Component, Mixins} from "vue-property-decorator";
+import TableStateController from "@/mixins/tableStateController";
+import RealtyType from "@/models/RealtyType";
+import SearchHelpers from "@/mixins/searchHelpers";
+import {AxiosResponse} from "axios";
+import {getModule} from "vuex-module-decorators";
+import Notification from "@/store/modules/notification";
+import Equipment from "@/models/Equipment";
+
+
+@Component({
+
+})
+export default class IndexRealtyType extends Mixins<TableStateController, SearchHelpers>(TableStateController, SearchHelpers) {
+  fields = [
+    {
+      key: 'selected',
+      label: 'Выбрано'
+    },
+    {
+      key: 'id',
+      label: 'ID',
+      sortable: true,
+      searchable: true
+    },
+    {
+      key: 'realtyTypeName',
+      label: 'Тип',
+      sortable: true,
+      searchable: true
+    },
+    {
+      key: 'name',
+      label: 'Название',
+      searchable: true,
+      sortable: true,
+    }
+  ]
+  items = [] as Array<RealtyType>
+
+  get selectionBtnText (): string { return this.selectedAllRows ? 'Снять выделение' : 'Выбрать все' }
+
+  onDelete (): void {
+    Equipment.destroy(this.selected.map(value => value.id as number)).then(() => {
+      getModule(Notification, this.$store).setData({ title: 'Удаление прошло успешно', variant: 'success' })
+      this.updateTableData();
+    })
+  }
+
+  updateTableData(): Promise<AxiosResponse<Array<RealtyType>>> {
+    return Equipment.getList(this.tableOptionsCleared)
+        .then(response => {
+          this.items = response.data
+
+          return response
+        })
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
